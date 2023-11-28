2a3d3442c7d5ac554edab400738921ad7146a38a
Oli Carlos <dev.olivercarlos@gmail.com>
2023-11-28 22:43:14 +0800

    add json resource macro for success and error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        JsonResponse::macro('success', function ($data = null, $message = null, $code = 200) {
+            return response()->json([
+                'success' => true,
+                'message' => $message,
+                'data' => $data,
+            ], $code);
+        });
+
+        JsonResponse::macro('error', function ($message = null, $code = 400) {
+            return response()->json([
+                'success' => false,
+                'message' => $message,
+            ], $code);
+        });
     }
 }
