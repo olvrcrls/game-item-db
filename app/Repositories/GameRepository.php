dcecac0ccb8fb859c4c95c7903841ea89b031348
Oli Carlos <dev.olivercarlos@gmail.com>
2023-11-28 22:44:24 +0800

    implement all function to game repository.

--- app/Repositories/GameRepository.php
+++ app/Repositories/GameRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Base\BaseRepository;
+use App\Http\Resources\GameResource;
 use App\Models\Game;
 
 class GameRepository extends BaseRepository
@@ -10,5 +11,15 @@ class GameRepository extends BaseRepository
     public function getModel(): Game
     {
         return new Game();
+    }
+
+    /**
+     * @param array<string> $columns
+     * @return GameResource
+     */
+    public function all($columns = '*'): GameResource
+    {
+        $data = $this->model->all($columns);
+        return new GameResource($data);
     }
 }
